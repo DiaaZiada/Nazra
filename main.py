f9784c7c4a73835391e6287126843f8e78b4190b
MohamedAliRashad <m.rashadnow@gmail.com>
2019-07-02 22:56:37 +0200

    Cleaning

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import argparse
 
-from Agent import *
-
 
 def main():
     # parse the path of the json config file
     arg_parser = argparse.ArgumentParser(description="Nazra Configuration")
-    arg_parser.add_argument('-c',
-                            '--config',
-                            metavar='config_json_file',
-                            default='None',
-                            help='The Configuration file in json format')
+    arg_parser.add_argument(
+        "-c",
+        "--config",
+        metavar="config_json_file",
+        default="None",
+        help="The Configuration file in json format",
+    )
     args = arg_parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
